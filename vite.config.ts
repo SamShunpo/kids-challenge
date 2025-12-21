2075c17be1290402ab330c2d8fb72ddfe803fcac
Samuel Gleize-Bourras <samuel@shunpo.fr>
2025-12-21 22:20:45 +0100

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/kids-challenge/', // Fixes 404 on GitHub Pages
   plugins: [
     react(),
     VitePWA({
